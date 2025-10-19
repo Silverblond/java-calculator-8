eb9fd041d16b2a69f3742e27625818ec260a83d9
silverblond <kdg23538@yu.ac.kr>
2025-10-19 22:49:58 +0900

    refactor: delete not used method

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -4,8 +4,4 @@ public class OutputView {
     public void printResult(int result) {
         System.out.println("결과 : " + result);
     }
-
-    public void printError(String errorMessage) {
-        System.out.println("[에러 발생]: " + errorMessage);
-    }
 }

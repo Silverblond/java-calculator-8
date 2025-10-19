e021946af0f6c5f1dcec0278c08921622811e055
silverblond <kdg23538@yu.ac.kr>
2025-10-19 22:14:40 +0900

    refactor(parse): seperate method parse and calculate logic

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -9,32 +9,38 @@ public class StringCalculator {
         if (input == null || input.isEmpty()) {
             return 0; //나중에 에러 핸들링 추가
         }
-        String[] numbers;
+        String[] stringNumbers = splitInput(input);
+
+        return sum(stringNumbers);
+    }
+
+    private String[] splitInput(String input) {
         if (input.startsWith("//")) {
             int delimiterEndIndex = input.indexOf("\n");
             if (delimiterEndIndex == -1) {
                 throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다.");
             }
+
             String customDelimiter = input.substring(2,delimiterEndIndex);
             String numbersPart = input.substring(delimiterEndIndex + 1);
-            numbers = numbersPart.split(customDelimiter);
+            return numbersPart.split(customDelimiter);
         }
-        else{
-            numbers = input.split(DEFAULT_DELIMITER);
-        }
+        return input.split(DEFAULT_DELIMITER);
+    }
 
-        int sum = 0;
-        for (String number : numbers) {
+    private int sum(String[] stringNumbers) {
+        int totalSum = 0;
+        for (String number : stringNumbers) {
             try {
                 int num = Integer.parseInt(number); //문자 -> 숫자 변환
                 if (num < 0) {
                     throw new IllegalArgumentException("음수는 허용되지 않습니다");
                 }
-                sum += num;
+                totalSum += num;
             } catch (NumberFormatException e) {
                 throw new IllegalArgumentException("숫자 이외의 값은 입력할 수 없습니다.");
             }
         }
-        return sum;
+        return totalSum;
     }
 }
